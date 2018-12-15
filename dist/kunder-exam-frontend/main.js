ddee7fe97d17a411899e758860b9f835e2aaa7be
Yuurastack <felipeaperezs@gmail.com>
2018-12-15 08:39:31 -0300

    herokuready\

--- dist/kunder-exam-frontend/main.js
+++ dist/kunder-exam-frontend/main.js
@@ -96,12 +96,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 /* harmony import */ var _components_home_home_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/home/home.component */ "./src/components/home/home.component.ts");
 /* harmony import */ var _banner_banner_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./banner/banner.component */ "./src/app/banner/banner.component.ts");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
 
 
 
@@ -118,7 +120,8 @@ var AppModule = /** @class */ (function () {
                 _banner_banner_component__WEBPACK_IMPORTED_MODULE_4__["BannerComponent"]
             ],
             imports: [
-                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"]
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
+                _angular_common_http__WEBPACK_IMPORTED_MODULE_5__["HttpClientModule"]
             ],
             providers: [],
             bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]]
@@ -138,7 +141,7 @@ var AppModule = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<p>\n  banner works!\n</p>\n"
+module.exports = "<!-- <div *ngIf=\"Product['show']\"> -->\n  <div [ngSwitch]=\"types\">\n    <div *ngSwitchCase=\"'plan'\" >\n      <div class=\"plan-header\" [ngStyle]=\"{'background-image': 'url('+ Product['backgroundImg'] + ')'}\" >\n        \n           <h1 class=\"col-12\">{{Product['productDescription']}}</h1>\n          <br>\n          <div class=\"button col-md-2\">\n          <a href=\"{{Product['link']}}\" class=\"btn btn-info\" role=\"button\" >{{Product['buttonText']}}<!-- <i class=\"fas fa-arrow-right\"></i> --></a>                 \n          </div>\n      </div>\n    </div>\n    <div *ngSwitchCase=\"'cellphone'\">\n      \n        <div class=\"product-header\" [ngStyle]=\"{'background-image': 'linear-gradient(to top right, #3333cc 0%, #006666 101%)'}\">\n            <img src=\"{{Product['productImg']}}\">\n              <h2><span >{{Product['productName']}}</span>\n                <br>{{Product['productDescription']}}\n              </h2>      \n\n        </div>\n\n    </div>\n  </div>\n<!-- </div> -->\n\n<style>\n\n</style>"
 
 /***/ }),
 
@@ -149,7 +152,7 @@ module.exports = "<p>\n  banner works!\n</p>\n"
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ""
+module.exports = ".plan-header {\n  text-align: center;\n  padding-top: 15%;\n  background-size: cover;\n  vertical-align: middle;\n  position: relative;\n  color: #fffcea;\n  font-size: 2vw !important;\n  min-height: 120px; }\n\n.product-header {\n  padding-top: 250px;\n  vertical-align: middle;\n  position: relative;\n  margin-bottom: 100px; }\n\n.button {\n  position: absolute;\n  left: 0;\n  right: 0;\n  margin-left: auto;\n  margin-right: auto;\n  bottom: 10%; }\n\nimg {\n  position: absolute;\n  left: 5%;\n  top: -100px;\n  z-index: 99;\n  max-width: 100%; }\n\nh2 {\n  font: 600 1vw 'Raleway', sans-serif;\n  color: rgba(255, 255, 255, 0.589);\n  text-align: center;\n  text-transform: uppercase;\n  letter-spacing: .5em;\n  position: absolute;\n  top: 15%;\n  width: 100%;\n  z-index: 100; }\n\nh1 {\n  font: 600 2vw 'Raleway', sans-serif;\n  color: rgba(255, 255, 255, 0.589);\n  text-align: center;\n  text-transform: uppercase;\n  letter-spacing: .5em;\n  position: absolute;\n  top: 25%;\n  width: 100%;\n  z-index: 100; }\n\nspan, span:after {\n  font-weight: 900;\n  color: #efedce;\n  white-space: nowrap;\n  display: inline-block;\n  position: relative;\n  letter-spacing: .1em;\n  padding: .2em 0 .25em 0;\n  width: 100%; }\n\nspan {\n  font-size: 5vw;\n  z-index: 100;\n  text-shadow: .04em .04em 0 #9cb8b3; }\n\n@media only screen and (max-width: 700px) {\n  .plan-header {\n    min-height: 130px; }\n  h2 {\n    font-size: 5vw; }\n  span {\n    font-size: 6.5vw;\n    z-index: 100;\n    text-shadow: .04em .04em 0 #9cb8b3; }\n  img {\n    position: absolute;\n    left: 15%;\n    top: -100px;\n    z-index: 99;\n    max-width: 100%; } }\n\n@media only screen and (max-width: 1100px) {\n  .plan-header {\n    min-height: 180px; }\n  h2 {\n    font-size: 3vw; } }\n"
 
 /***/ }),
 
@@ -164,6 +167,7 @@ module.exports = ""
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "BannerComponent", function() { return BannerComponent; });
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -174,18 +178,28 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
+
 var BannerComponent = /** @class */ (function () {
-    function BannerComponent() {
+    function BannerComponent(http) {
+        this.http = http;
+        this.desc1 = '';
+        this.desc2 = '';
     }
     BannerComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.http.get('https://hack.kunderlabs.com/exam/telecom/api/products/new').subscribe(function (data) {
+            _this.Product = data['product'];
+            console.log(_this.Product);
+            _this.types = _this.Product['type'];
+        });
     };
     BannerComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
             selector: 'app-banner',
             template: __webpack_require__(/*! ./banner.component.html */ "./src/app/banner/banner.component.html"),
-            styles: [__webpack_require__(/*! ./banner.component.scss */ "./src/app/banner/banner.component.scss")]
+            styles: [__webpack_require__(/*! ./banner.component.scss */ "./src/app/banner/banner.component.scss")],
         }),
-        __metadata("design:paramtypes", [])
+        __metadata("design:paramtypes", [_angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClient"]])
     ], BannerComponent);
     return BannerComponent;
 }());
@@ -245,7 +259,7 @@ var HomeComponent = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!-- Generated template for the HomeComponent component -->\n<div>\n    <div>\n      <div class=\"kunderLogo\">\n        <span class=\"telecom\">telecoms</span>\n      </div>\n      <div>\n        <button ion-button icon-only clear class=\"menuButton\"></button>\n      </div>\n    </div>\n  </div>\n<content no-padding scroll=\"false\">\n  <div class=\"header\">\n    \n  </div>\n  <div class=\"buttons\">\n    <button class=\"buttonsHome buttonConsumo\">\n      <span>Consumo</span>\n    </button>\n    <button class=\"buttonsHome buttonPlan\">\n      <span>Mi Plan</span>\n    </button>\n    <button class=\"buttonsHome buttonBolsas\">\n      <span>Mis bolsas</span>\n    </button>\n    <button class=\"buttonsHome buttonBeneficios\">\n      <span>Beneficios</span>\n    </button>\n  </div>\n</content>\n<footer class=\"informationFooter\">\n      <a class=\"shuffleLeft\">Sucursales</a>\n      <a class=\"shuffleRight\">Consultas</a>\n</footer>\n"
+module.exports = "<!-- Generated template for the HomeComponent component -->\n<div>\n    <div>\n      <div class=\"kunderLogo\">\n        <span class=\"telecom\">telecoms</span>\n      </div>\n      <div>\n        <button ion-button icon-only clear class=\"menuButton\"></button>\n      </div>\n    </div>\n  </div>\n<content no-padding scroll=\"false\">\n  <div class=\"header1\">\n    \n  </div>\n  <app-banner></app-banner>\n  <div class=\"buttons\">\n    <button class=\"buttonsHome buttonConsumo\">\n      <span>Consumo</span>\n    </button>\n    <button class=\"buttonsHome buttonPlan\">\n      <span>Mi Plan</span>\n    </button>\n    <button class=\"buttonsHome buttonBolsas\">\n      <span>Mis bolsas</span>\n    </button>\n    <button class=\"buttonsHome buttonBeneficios\">\n      <span>Beneficios</span>\n    </button>\n  </div>\n</content>\n<footer class=\"informationFooter\">\n      <a class=\"shuffleLeft\">Sucursales</a>\n      <a class=\"shuffleRight\">Consultas</a>\n</footer>\n"
 
 /***/ }),
 
